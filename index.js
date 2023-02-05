1007fdfad82bcf6955675787982aa27276921211
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-05 14:22:21 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(records) {
+    const win = records.find(item => item.result === 'W');
+  
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
+  
